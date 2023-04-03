fd584f66a8a05d20657b2c25d9b6680659472f69
angeldragieww <angeldragieww@gmail.com>
2023-04-03 11:50:36 +0300

    Changed the name of the solution

---
+++ MarketPlace/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Models.OrderModels.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        private readonly IOrderService orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        [HttpGet]
+        public void Test()
+        {
+            orderService.Test();
+        }
+    }
+}
