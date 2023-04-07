f19721c468a70f6f434b521ea8dab4e309a6420e
angeldragieww <angeldragieww@gmail.com>
2023-04-07 16:56:19 +0300

    Small changes in validation

--- Application/Helpers/Validators/ProductUpdateValidator.cs
+++ Application/Helpers/Validators/ProductUpdateValidator.cs
@@ -12,7 +12,8 @@ namespace Application.Helpers.Validators
     {
         public ProductUpdateValidator()
         {
-            RuleFor(p => p.FullName).NotNull().NotEmpty().WithMessage("Product name cannot be empty or null!");
+            RuleFor(p => p.Code).NotEmpty().NotNull().WithMessage("Product code is required!");
+            RuleFor(p => p.FullName).NotNull().NotEmpty().WithMessage("Product name is required");
             RuleFor(p => p.Price).GreaterThanOrEqualTo(0).WithMessage("Price must be positive number");
             RuleFor(p => p.SaleQty).GreaterThanOrEqualTo(0).WithMessage("Sale quantity must be positive number");
             RuleFor(p => p.CombinedQty).GreaterThanOrEqualTo(p => p.SaleQty).WithMessage("Combined quantity must be greater than or equal to sale quantity");
