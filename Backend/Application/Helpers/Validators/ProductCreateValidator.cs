21eb8e528f55fbc13108e4efa1d9cda5204e1520
ADragiev <ADragiev@vsgbg.com>
2023-04-18 15:48:39 +0300

    Small changes in validation.

--- Backend/Application/Helpers/Validators/ProductCreateValidator.cs
+++ Backend/Application/Helpers/Validators/ProductCreateValidator.cs
@@ -13,7 +13,7 @@ namespace Application.Helpers.Validators
         public ProductCreateValidator()
         {
             RuleFor(p=>p.Code).NotEmpty().WithMessage("Product code is required!").NotNull().WithMessage("Product code is required!");
-            RuleFor(p => p.FullName).NotNull().WithMessage("Product code is required!").NotEmpty().WithMessage("Product name is required");
+            RuleFor(p => p.FullName).MaximumLength(50).NotNull().WithMessage("Product code is required!").NotEmpty().WithMessage("Product name is required");
             RuleFor(p => p.Price).GreaterThanOrEqualTo(0).WithMessage("Price must be positive number");
             RuleFor(p => p.SaleQty).GreaterThanOrEqualTo(0).WithMessage("Sale quantity must be positive number");
             RuleFor(p => p.CombinedQty).GreaterThanOrEqualTo(p=>p.SaleQty).WithMessage("Combined quantity must be greater than or equal to sale quantity");
