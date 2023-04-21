5475f535ba246b86b5295851a568dd56151133f7
ADragiev <ADragiev@vsgbg.com>
2023-04-21 10:07:36 +0300

    Added location repo.

---
+++ Backend/Infrastructure/Repositories/LocationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.Models.GenericRepo;
+using Application.Models.LocationModels.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class LocationRepository : GenericRepository<Location>, ILocationRepository
+    {
+        public LocationRepository(IMarketPlaceContext marketPlaceContext)
+            : base(marketPlaceContext)
+        {
+        }
+    }
+}
