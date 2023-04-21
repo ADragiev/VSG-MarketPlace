d0ea814be52cdb3b223ee5aa93af147a3308517f
ADragiev <ADragiev@vsgbg.com>
2023-04-21 13:31:27 +0300

    Now getting database name from default connection string instead of hard coding it in database creation class.

--- Backend/Infrastructure/Migrations/Database.cs
+++ Backend/Infrastructure/Migrations/Database.cs
@@ -12,20 +12,24 @@ namespace Infrastructure.Migrations
     public class Database
     {
         private readonly string masterConnectionString;
+        private readonly string databaseName;
 
         public Database(IConfiguration config)
         {
             masterConnectionString = config.GetConnectionString("MasterConnection");
+
+            var csb = new SqlConnectionStringBuilder(config.GetConnectionString("DefaultConnection"));
+            this.databaseName = csb.InitialCatalog;
         }
         public void CreateDatabase()
         {
-            var query = "SELECT * FROM sys.databases WHERE name = 'MarketPlace'";
+            var query = $"SELECT * FROM sys.databases WHERE name = '@databaseName'";
 
             using (var connection = new SqlConnection(masterConnectionString))
             {
-                var records = connection.Query(query);
+                var records = connection.Query(query, new { databaseName });
                 if (!records.Any())
-                    connection.Execute($"CREATE DATABASE MarketPlace");
+                    connection.Execute($"CREATE DATABASE {databaseName}");
             }
         }
     }
