ff3163d4d29979982e2b0aded7b01950a1e9ae99
Stiliyan Stoyanov <SStoyanov@vsgbg.com>
2023-04-27 10:01:03 +0300

    Better formated js files

--- Frontend/src/Marketplace/detailsPopup.js
+++ Frontend/src/Marketplace/detailsPopup.js
@@ -3,19 +3,18 @@ import { createModal } from "./createModal.js";
 const modal = document.querySelector(".modal");
 export async function showModal(cardDiv, product) {
   const productImage = cardDiv.querySelector(".product-image");
-   productImage.addEventListener("click", async (e) => {
-      e.preventDefault();
-      await createModal(product);
-      modal.style.display = "flex";
-      modal.addEventListener('click', (e)=>{
-        if (e.target.className == 'modal') {
-      modal.style.display = "none";
-      modal.innerHTML = "";  
-
-        }
-      },)
-      closeModal()
+  productImage.addEventListener("click", async (e) => {
+    e.preventDefault();
+    await createModal(product);
+    modal.style.display = "flex";
+    modal.addEventListener("click", (e) => {
+      if (e.target.className == "modal") {
+        modal.style.display = "none";
+        modal.innerHTML = "";
+      }
     });
+    closeModal();
+  });
 }
 export function closeModal() {
   document
@@ -25,6 +24,6 @@ export function closeModal() {
 
 function closing(e) {
   e?.preventDefault();
-    e.target.parentElement.remove()
-    modal.style.display = "none";
+  e.target.parentElement.remove();
+  modal.style.display = "none";
 }
