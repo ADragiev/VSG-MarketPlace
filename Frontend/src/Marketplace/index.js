22da2ed800ae048fbcba9c6d7f4eea57fa617b26
Stiliyan Stoyanov <SStoyanov@vsgbg.com>
2023-04-27 09:45:01 +0300

    Fixed hover on products on Marketplace page

--- Frontend/src/Marketplace/index.js
+++ Frontend/src/Marketplace/index.js
@@ -8,7 +8,16 @@ import './createModal.js';
 import { createCard } from "./card.js";
 import { loadProducts } from "../global/itemsService.js"
 
+
+
+const main = document.querySelector('#main-list-wrapper')
+
 const productsData = await loadProducts();
-productsData.forEach((product) => {
-  createCard(product);
-});
+if (productsData.length > 0) {
+  productsData.forEach((product) => {
+    createCard(product);
+  });
+}
+else{
+  main.innerHTML = `<span id="no-items-span">No items for sale</span>`
+}
