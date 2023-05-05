58d5389b5707fe354a64f705c8a49eef7a33d250
ADragiev <ADragiev@vsgbg.com>
2023-05-05 15:04:11 +0300

    Added authentication. Validating jwt token coming from azure identity.

--- Backend/MarketPlace/Program.cs
+++ Backend/MarketPlace/Program.cs
@@ -1,6 +1,9 @@
 using Application.Helpers.Configurations;
 using Application.Helpers.Middlewares;
 using Infrastructure.Configurations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NLog.Web;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,18 +27,55 @@ builder.Services.AddConfigurationInfrastructureLayer();
 builder.Services.AddConfigurationMigration();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+    // Add JWT authentication
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter JWT Bearer token",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+    options.AddSecurityDefinition("Bearer", securityScheme);
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securityScheme, new[] { "Bearer" } }
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://login.microsoftonline.com/50ae1bf7-d359-4aff-91ac-b084dc52111e/v2.0";//tenantId
+        options.Audience = "86ceffd4-8632-4677-bbb6-e7badafa26ec";//clientId
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
-            {
-                options.AddPolicy(name: "CORSPolicy", policy =>
-                {
-                    policy
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowAnyOrigin();
-                });
-         });
+ {
+     options.AddPolicy(name: "CORSPolicy", policy =>
+     {
+         policy
+     .AllowAnyMethod()
+     .AllowAnyHeader()
+     .AllowAnyOrigin();
+     });
+ });
 
 var app = builder.Build();
 
@@ -52,6 +92,7 @@ app.CreateViewsAndFunctions();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
