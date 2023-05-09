c6daf24020affed5b96f2e39c8a9947b175a5deb
ADragiev <ADragiev@vsgbg.com>
2023-05-09 10:05:25 +0300

    Moved configuration for swagger gen in a separate class.

--- Backend/MarketPlace/Program.cs
+++ Backend/MarketPlace/Program.cs
@@ -2,9 +2,11 @@ using Application.Helpers.Configurations;
 using Application.Helpers.Middlewares;
 using Infrastructure.Configurations;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NLog.Web;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,46 +14,18 @@ builder.Host.ConfigureLogging(logging =>
 {
     logging.ClearProviders();
 }).UseNLog();
-
 IConfigurationRoot config = new ConfigurationBuilder()
     .AddJsonFile(path: "appsettings.json").Build();
 NLog.Extensions.Logging.ConfigSettingLayoutRenderer.DefaultConfiguration = config;
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
 builder.Services.AddConfigurationApplicationLayer();
 builder.Services.AddConfigurationInfrastructureLayer();
 builder.Services.AddConfigurationMigration();
-
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigurationSwaggerOptions>();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
 
-    // Add JWT authentication
-    var securityScheme = new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Description = "Enter JWT Bearer token",
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
-    options.AddSecurityDefinition("Bearer", securityScheme);
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        { securityScheme, new[] { "Bearer" } }
-    });
-});
 
 builder.Services.AddAuthentication(options =>
 {
@@ -63,7 +37,6 @@ builder.Services.AddAuthentication(options =>
         options.Authority = "https://login.microsoftonline.com/50ae1bf7-d359-4aff-91ac-b084dc52111e/v2.0";//tenantId
         options.Audience = "86ceffd4-8632-4677-bbb6-e7badafa26ec";//clientId
     });
-
 builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
