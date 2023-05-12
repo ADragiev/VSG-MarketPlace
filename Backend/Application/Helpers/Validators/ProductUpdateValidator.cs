c9c55cbfd0c31370f87ffd0fd95317d3e9a5b159
ADragiev <ADragiev@vsgbg.com>
2023-05-12 12:39:51 +0300

    Refactored validators for products

--- Backend/Application/Helpers/Validators/ProductUpdateValidator.cs
+++ Backend/Application/Helpers/Validators/ProductUpdateValidator.cs
@@ -21,17 +21,34 @@ namespace Application.Helpers.Validators
             this.categoryRepo = categoryRepo;
             this.locationRepo = locationRepo;
 
-            RuleFor(p => p.Code).MaximumLength(50).WithMessage("Product code length cannot be over 50 characters!").NotNull().NotEmpty().WithMessage("Product code is required!");
-            RuleFor(p => p.Name).MaximumLength(100).WithMessage("Product name length cannot be over 100 characters!").NotNull().NotEmpty().WithMessage("Product name is required");
-            RuleFor(p => p.Price).GreaterThanOrEqualTo(0).WithMessage("Price must be positive number");
-            RuleFor(p => p.SaleQty).GreaterThanOrEqualTo(0).WithMessage("Sale quantity must be positive number");
-            RuleFor(p => p.Description).MaximumLength(200).WithMessage("Description length cannot be over 200 characters!");
-            RuleFor(p => p.CombinedQty).GreaterThanOrEqualTo(p => p.SaleQty).WithMessage("Combined quantity must be greater than or equal to sale quantity");
+            RuleFor(p => p.Code)
+               .MaximumLength(50).WithMessage("Product code length cannot be over 50 characters!")
+               .NotNull().WithMessage("Product code is required!")
+               .NotEmpty().WithMessage("Product code is required!");
+
+            RuleFor(p => p.Name)
+                .NotNull().WithMessage("Product name is required")
+                .NotEmpty().WithMessage("Product name is required")
+                .MaximumLength(100).WithMessage("Product name length cannot be over 100 characters!");
+
+            RuleFor(p => p.Price)
+                .GreaterThanOrEqualTo(0).WithMessage("Price must be positive number");
+
+            RuleFor(p => p.SaleQty)
+                .GreaterThanOrEqualTo(0).WithMessage("Sale quantity must be positive number");
+
+            RuleFor(p => p.CombinedQty)
+                .GreaterThanOrEqualTo(p => p.SaleQty).WithMessage("Combined quantity must be greater than or equal to sale quantity");
+
+            RuleFor(p => p.Description)
+                .MaximumLength(200).WithMessage("Description length cannot be over 200 characters!");
+
             RuleFor(p => p.CategoryId).MustAsync(async (categoryId, cancellation) =>
             {
                 var category = await categoryRepo.GetByIdAsync(categoryId);
                 return category != null;
             }).WithMessage("Category Id is not valid!");
+
             RuleFor(p => p.LocationId).MustAsync(async (locationId, cancellation) =>
             {
                 var location = await locationRepo.GetByIdAsync(locationId);
