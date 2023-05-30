be79ef184428b7709839d1641e6fa391579663a3
ADragiev <ADragiev@vsgbg.com>
2023-05-30 13:20:48 +0300

    Simplifiend the api configuration.

--- Backend/MarketPlace/Configurations/ConfigurationApiLayer.cs
+++ Backend/MarketPlace/Configurations/ConfigurationApiLayer.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Helpers.Constants;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace MarketPlace.Configurations
@@ -10,8 +11,32 @@ namespace MarketPlace.Configurations
         public static IServiceCollection AddConfigurationApiLayer(this IServiceCollection services, IConfiguration config)
         {
             services.AddControllers();
-            services.AddSwaggerGen();
-            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigurationSwaggerOptions>();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "MarketPlace", Version = "v1" });
+
+                // Add JWT authentication
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter JWT Bearer token",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                options.AddSecurityDefinition("Bearer", securityScheme);
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { securityScheme, new[] { "Bearer" } }
+                });
+            });
+
             services.AddEndpointsApiExplorer();
 
 
