831ac57c1678e0fa874f282e5485aa6f72b4b34b
ADragiev <ADragiev@vsgbg.com>
2023-06-07 12:53:18 +0300

    Added check constraint migration for combined qty to be bigger than other two

---
+++ Backend/Infrastructure/Migrations/JuneMigrations/202306071249_AddCheckConstraintForCombinedQtyToBeBiggerThanOtherTwo.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Migrations.JuneMigrations
+{
+    [Migration(202306071249)]
+    public class AddCheckConstraintForCombinedQtyToBeBiggerThanOtherTwo : Migration
+    {
+        public override void Down()
+        {
+            Execute.Sql("ALTER TABLE Product DROP CONSTRAINT CK_CombinedQtyBiggerThanOtherTwo_ProductTable");
+        }
+
+        public override void Up()
+        {
+            Execute.Sql("ALTER TABLE Product ADD CONSTRAINT CK_CombinedQtyBiggerThanOtherTwo_ProductTable CHECK (CombinedQty >= (LendQty + SaleQty))");
+        }
+    }
+}
