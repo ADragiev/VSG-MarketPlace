20b6b1712e7f67fc035e6e07b268716189eacf7b
ADragiev <ADragiev@vsgbg.com>
2023-06-07 09:36:53 +0300

    Added migration for unique index for code and location.

---
+++ Backend/Infrastructure/Migrations/JuneMigrations/202306070914_AddUniqueIndexOnCodeAndLocation.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using FluentMigrator.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Migrations.JuneMigrations
+{
+    [Migration(202306070914)]
+    public class AddUniqueIndexOnCodeAndLocation : Migration
+    {
+        public override void Down()
+        {
+            Delete.Index("IX_Product_Code_Location").OnTable("Product");
+        }
+
+        public override void Up()
+        {
+            Create.Index("IX_Product_Code_Location")
+             .OnTable("Product").InSchema("dbo")
+             .OnColumn("Code").Ascending()
+             .OnColumn("LocationId").Ascending()
+             .WithOptions().Unique()
+             .WithOptions().Filter("IsDeleted = 0");
+        }
+    }
+}
